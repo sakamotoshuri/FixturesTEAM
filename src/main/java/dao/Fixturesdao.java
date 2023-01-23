e7e74ffa48c7a62a62bbc5195eda64165f27366b
sasakitoma <t.sasaki.sys22@morijyobi.ac.jp>
2023-01-23 11:04:38 +0900

    IDとカテゴリ検索機能のservletと機能をdaoに追記menuのjspも書きました

--- src/main/java/dao/Fixturesdao.java
+++ src/main/java/dao/Fixturesdao.java
@@ -4,7 +4,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.Fixturesdto;
 
 public class Fixturesdao {
 	private static Connection getConnection() throws URISyntaxException, SQLException {
@@ -23,6 +29,137 @@ public class Fixturesdao {
 	}
 	
 	
+	public static List<Fixturesdto> SearchProductName(String serch) {
+		String serchname="%"+serch+"%";
+		
+		String sql = "select * from fixturres where fixtures_name LIKE ? ";
+		List<Fixturesdto>result=new ArrayList<>();
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1,serchname);
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					
+					int fixtures_id = rs.getInt("fixtures_id");
+					String fixtures_name = rs.getString("fixtures_name");
+					int quantity = rs.getInt("quantity");
+					String category = rs.getString("category");
+					String content = rs.getString("content");
+					Fixturesdto fixtures = new Fixturesdto( fixtures_id,fixtures_name , quantity ,category, content);
+					
+					result.add(fixtures);
+					
+										
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件商品を検索しました。");
+		}
+		return result;
+	}
+	public static List<Fixturesdto> SearchFixturesCategory(String serch) {
+		String serchname="%"+serch+"%";
+		
+		String sql = "select * from fixtures where category LIKE ? ";
+		List<Fixturesdto>result=new ArrayList<>();
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1,serchname);
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					
+					int fixtures_id = rs.getInt("fixtures_id");
+					String fixtures_name = rs.getString("fixtures_name");
+					int quantity = rs.getInt("quantity");
+					String category = rs.getString("category");
+					String content = rs.getString("content");
+					Fixturesdto fixtures = new Fixturesdto( fixtures_id,fixtures_name , quantity ,category, content);
+					
+					result.add(fixtures);
+					
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件商品を検索しました。");
+		}
+		return result;
+	}
+	
+	public static List<Fixturesdto> SearchProductId(int serch) {
+		int serchname=serch;
+		
+		String sql = "select * from fixtures where fixtures_id = ? ";
+		List<Fixturesdto>result=new ArrayList<>();
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setInt(1,serchname);
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					
+					int fixtures_id = rs.getInt("fixtures_id");
+					String fixtures_name = rs.getString("fixtures_name");
+					int quantity = rs.getInt("quantity");
+					String category = rs.getString("category");
+					String content = rs.getString("content");
+					Fixturesdto fixtures = new Fixturesdto( fixtures_id,fixtures_name , quantity ,category, content);
+					
+					result.add(fixtures);
+					
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件商品を検索しました。");
+		}
+		return result;
+	}
+	public static List<Fixturesdto> selectAllFixtures() {
+		String sql = "select * from fixtures";
+		List<Fixturesdto>result=new ArrayList<>();
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					
+					int fixtures_id = rs.getInt("fixtures_id");
+					String fixtures_name = rs.getString("fixtures_name");
+					int quantity = rs.getInt("quantity");
+					String category = rs.getString("category");
+					String content = rs.getString("content");
+					Fixturesdto fixtures = new Fixturesdto( fixtures_id,fixtures_name , quantity ,category, content);
+					
+					result.add(fixtures);
+					
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件商品を検索しました。");
+		}
+		return result;
+	}
 	
 	
 }
---
+++ src/main/webapp/WEB-INF/view/menu.jsp
@@ -0,0 +1,86 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+     <%@ page import = "dto.Fixturesdto" %>
+      <%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div class=exclamation2 >
+	備品管理
+	</div>
+     <nav class="navbar navbar-expand-lg navbar-dark " style =" background-color: #48CFAE;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="RegesterProduct">登録</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  カテゴリ
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="CategorySerch?category=本・コミック・雑誌">本・コミック・雑誌</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=DVD・ミュージック・ゲーム">DVD・ミュージック・ゲーム</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=家電・カメラ・AV機器">家電・カメラ・AV機器</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=パソコン・オフィス用品">パソコン・オフィス用品</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=スマートホーム">スマートホーム</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=ホーム＆キッチン・ペット・DIY">ホーム＆キッチン・ペット・DIY</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=食品・飲料・お酒">食品・飲料・お酒</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=ドラッグストア・ビューティー">ドラッグストア・ビューティー</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=べビー・おもちゃ・ホビー">べビー・おもちゃ・ホビー</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=服・シューズ・バッグ・腕時計">服・シューズ・バッグ・腕時計</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=スポーツ＆アウトドア">スポーツ＆アウトドア</a></li>
+                  <li><a class="dropdown-item" href="CategorySerch?category=車＆バイク・産業・研究開発">車＆バイク・産業・研究開発</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" action="ProductSerchExcute" method="post">
+             <input class="form-control me-2" type="text" name = "product_name"placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>	
+
+
+<table border="1">
+
+	
+		<tr>
+			<th>id</th>
+			<th>備品名</th>
+			<th>個数</th>
+			<th>カテゴリ</th>
+		</tr>
+	
+		<%
+	List<Fixturesdto> lista = (ArrayList<Fixturesdto>)request.getAttribute("list");
+	for(Fixturesdto s : lista) {
+		%>
+	
+		<tr>
+			<td><a href = "ProductIdSerch?id=<%=s.getFixtures_id() %>"><%=s.getFixtures_id()%></a></td>
+			<td><%=s.getFixtures_name()%></td>
+			<td><%=s.getQuantity()%></td>
+			<td><a href = "CategorySerch?category=<%=s.getCategory() %>"><%=s.getCategory() %></a></td>
+		</tr>
+
+	<%} %>
+	</table>
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
