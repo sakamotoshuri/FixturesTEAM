e0c7b7fcbc89198d3c95befa3b4753ffb997d7eb
sakamoto <s.sakamoto.sys22@morijyobi.ac.jp>
2023-01-24 10:33:17 +0900

    登録機能DAO

--- src/main/java/dao/Fixturesdao.java
+++ src/main/java/dao/Fixturesdao.java
@@ -4,7 +4,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import dto.Fixtures;
 
 public class Fixturesdao {
 	private static Connection getConnection() throws URISyntaxException, SQLException {
@@ -22,7 +25,53 @@ public class Fixturesdao {
 	    return DriverManager.getConnection(dbUrl, username, password);
 	}
 	
-	
+	// 引数の Product インスタンスを元にデータを1件INSERTするメソッド
+	public static int registerFixtures(Fixtures fixt) {
+		
+		String sql = "INSERT INTO Fixtures VALUES(default, ?, ?, ?, ?)";
+		// return用の変数
+		int result = 0;
+		
+		try (
+				Connection con = getConnection();	// DB接続
+				PreparedStatement pstmt = con.prepareStatement(sql);			// 構文解析
+				){
+			pstmt.setString(1, fixt.getFixtures_name());
+			pstmt.setInt(2, fixt.getQuantity());
+			pstmt.setString(3, fixt.getCategory());
+			pstmt.setString(4, fixt.getContent());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
+	//削除
+	public static int DeleteFixtures(int Fixtures_id) {
+		
+		String sql = "DELETE FROM Fixtures WHERE fixtures_id = ?";
+		int result = 0;
+		try (
+				Connection con = getConnection();	// DB接続
+				PreparedStatement pstmt = con.prepareStatement(sql);			// 構文解析
+				){
+			
+			pstmt.setInt(1,Fixtures_id );
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件削除しました。");
+		}
+		return  result;
+	}
 	
 	
 }
